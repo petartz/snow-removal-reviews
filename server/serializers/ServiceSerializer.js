54cc68370f147ab4e9bf6fc41a821929b8ef5ac2
Petar Tzonevski <Pete@Petars-MBP.fios-router.home>
2022-02-02 16:45:05 -0500

    remove console logs and empty lines

--- server/serializers/ServiceSerializer.js
+++ server/serializers/ServiceSerializer.js
@@ -9,27 +9,17 @@ class ServiceSerializer{
     for(const attribute of allowedAttributes){
       serializedService[attribute] = service[attribute]
     }
-
     const relatedReviews = await service.$relatedQuery('reviews')
-
     relatedReviews.forEach(review => {
-      console.log(review)
-      console.log(review.rating)
       ratingSum += review.rating
     });
-
-    console.log(`rating sum = ${ratingSum}`)
-
     serializedService.rating = (ratingSum / relatedReviews.length)
-
-    console.log(`related reviews = ${relatedReviews}`)
     return serializedService
   }
 
   static async getDetails(service){
     const allowedAttributes = ["id", "name", "number", "email", "websiteUrl", "photoUrl"]
     let ratingSum = 0;
-
     let serializedService = {}
     for(const attribute of allowedAttributes){
       serializedService[attribute] = service[attribute]
