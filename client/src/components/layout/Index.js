1ef9e79e3ebd7fc7ae7d94e0c6d6f3b09af39542
JCWoodworker <jfc3303@gmail.com>
2022-02-02 10:09:29 -0500

    Co-authored-by: ltfang <ltfang@users.noreply.github.com>

--- client/src/components/layout/Index.js
+++ client/src/components/layout/Index.js
@@ -1,72 +1,62 @@
-import React, {useEffect, useState} from "react"
-import ServiceTile from "./ServiceTile.js"
-import AddServicesForm from "./AddServicesForm.js"
+import React, { useEffect, useState } from "react";
+import ServiceTile from "./ServiceTile.js";
+import AddServicesForm from "./AddServicesForm.js";
 
-const Index = (props) =>{
-    const [services, setServices] = useState([])
+const Index = (props) => {
+  const [services, setServices] = useState([]);
 
-    const fetchServices = async () => {
-        try{
-            const response = await fetch("/api/v1/services")
-            if(!response.ok){
-                throw (new Error(`${response.status} ${response.statusText}`))
-            }
-            const body = await response.json()
-            setServices(body.services)
-
-        }catch(error){
-            return(console.error(`Error in fetch: ${error.message}`))
-        }
+  const fetchServices = async () => {
+    try {
+      const response = await fetch("/api/v1/services");
+      if (!response.ok) {
+        throw new Error(`${response.status} ${response.statusText}`);
+      }
+      const body = await response.json();
+      setServices(body.services);
+    } catch (error) {
+      return console.error(`Error in fetch: ${error.message}`);
     }
+  };
 
-    useEffect(()=>{
-        fetchServices()
-    }, [])
+  useEffect(() => {
+    fetchServices();
+  }, []);
 
-    const postService = async (newServiceData) => {
-        try {
-            
-            const response = await fetch("/api/v1/services", {
-                method: "POST",
-                headers: new Headers({
-                    "Content-Type": "application/json"
-                }),
-                body: JSON.stringify(newServiceData)
-            })
-            debugger
-            if (!response.ok) {
-                const errorMessage = `${response.status} (${response.statusText})`
-                const error = new Error(errorMessage)
-                throw (error)
-            } else {
-                const body = await response.json()
-                // const updatedServices = services.concat(body.service)
-                setServices([ ...services, body.service])
-            }
-
-        } catch (error) {
-            console.error(`Error in fetch ${error.message}`)
-        }
+  const postService = async (newServiceData) => {
+    try {
+      const response = await fetch("/api/v1/services", {
+        method: "POST",
+        headers: new Headers({
+          "Content-Type": "application/json",
+        }),
+        body: JSON.stringify(newServiceData),
+      });
+      debugger;
+      if (!response.ok) {
+        const errorMessage = `${response.status} (${response.statusText})`;
+        const error = new Error(errorMessage);
+        throw error;
+      } else {
+        const body = await response.json();
+        // const updatedServices = services.concat(body.service)
+        setServices([...services, body.service]);
+      }
+    } catch (error) {
+      console.error(`Error in fetch ${error.message}`);
     }
+  };
 
-    const servicesTiles = services.map(service =>{
-        return (
-            <ServiceTile
-                key={service.id}
-                service={service}
-            />
-        )
-    })
+  const servicesTiles = services.map((service) => {
+    return <ServiceTile key={service.id} service={service} />;
+  });
 
-    return (
-        <div>
-            <h1>Snow Removal Service Reviews!!</h1>
-            {servicesTiles}
-            <AddServicesForm 
-                postService = {postService}
-            />
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Snow Removal Service Reviews!!</h1>
+      {servicesTiles}
+      <AddServicesForm postService={postService} />
+    </div>
+  );
+};
 
-export default Index
+export default Index;
--- server/src/routes/api/v1/servicesRouter.js
+++ server/src/routes/api/v1/servicesRouter.js
@@ -1,30 +1,29 @@
-import express from "express"
-import Service from "../../../models/Service.js"
-import ServiceSerializer from "../../../../serializers/ServiceSerializer.js"
+import express from "express";
+import Service from "../../../models/Service.js";
+import ServiceSerializer from "../../../../serializers/ServiceSerializer.js";
 
-const serviceRouter = new express.Router()
+const serviceRouter = new express.Router();
 
-serviceRouter.get("/", async (req,res) =>{
-    try{
-        const services = await Service.query()
-        const serializedServices = services.map(service =>{
-            return ServiceSerializer.getSummary(service)
-        })
-        return res.status(200).json({ services: serializedServices })
-    }catch(error){
-        return res.status(500).json({errors:error})
-    }
-
-})
+serviceRouter.get("/", async (req, res) => {
+  try {
+    const services = await Service.query();
+    const serializedServices = services.map((service) => {
+      return ServiceSerializer.getSummary(service);
+    });
+    return res.status(200).json({ services: serializedServices });
+  } catch (error) {
+    return res.status(500).json({ errors: error });
+  }
+});
 
 serviceRouter.post("/", async (req, res) => {
-    try {
-        debugger
-        const newService = await Service.query().insertAndFetch(req.body)
-        return res.status(201).json({ service: newService })
-    } catch(error) {
-        return res.status(500).json({ errors: error })
-    }
-})
+  try {
+    debugger;
+    const newService = await Service.query().insertAndFetch(req.body);
+    return res.status(201).json({ service: newService });
+  } catch (error) {
+    return res.status(500).json({ errors: error });
+  }
+});
 
-export default serviceRouter
+export default serviceRouter;
