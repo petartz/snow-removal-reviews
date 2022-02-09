c117852d72def90aa2218114cf76e596029f0bb3
pcadle <paul.cadle1@gmail.com>
2022-02-09 16:43:08 -0500

    initial weather test

--- client/src/components/layout/TopBar.js
+++ client/src/components/layout/TopBar.js
@@ -1,8 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import SignOutButton from "../authentication/SignOutButton";
 
 const TopBar = ({ user }) => {
+  // const [location, setLocation] = useState({
+  //   lat: 0,
+  //   long: 0
+  // })
+  // const [forecast, setForecast] = useState({
+  //   city: '',
+  //   temp: ''
+  // })
+  // const successfulLookup = async position => {
+  //   const { latitude, longitude } = position.coords
+  //   console.log(latitude, longitude)
+  //   // setLocation({lat: latitude, long: longitude})
+  //   try{
+  //     const response = await fetch (`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=f2f86640139e21f2e625867855478652`)
+  //     const body = await response.json()
+  //     // setForecast({city: body.city.name})
+  //     console.log(body.city.name)
+  //     debugger
+  //     if(!response.ok){
+  //       throw new Error(`${response.status} ${response.statusText}`)
+  //     }
+  //   }catch(error){
+  //     console.log(error)
+  //   }
+  // }
+  // window.navigator.geolocation.getCurrentPosition(successfulLookup, console.log)
+
   const unauthenticatedListItems = [
     <li key="sign-in">
       <Link to="/user-sessions/new">Sign In</Link>
@@ -28,6 +55,7 @@ const TopBar = ({ user }) => {
           <li>
             <Link to="/">Home</Link>
           </li>
+          {/* <li>{location.latitude} {location.longitude}</li> */}
         </ul>
       </div>
       <div className="top-bar-right">
