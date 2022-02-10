492a036cc6381bbe15b0361e0e1cafae53ead8ef
Petar Tzonevski <Pete@Petars-MBP.fios-router.home>
2022-02-10 17:11:10 -0500

    touched up font-awesome dependency, near final version

---
+++ server/src/routes/api/v1/voteReviewsRouter.js.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import { Vote } from '../../../models/index.js'
+import VoteSerializer from '../../../../serializers/VoteSerializer.js'
+
+const voteReviewsRouter = new express.Router({ mergeParams:true })
+
+voteReviewsRouter.post('/', async (req, res) => {
+  try {
+    const { voteValue } = req.body
+    const { reviewId } = req.params
+    const priorVote = await VoteSerializer.getPriorVote(reviewId, req.user.id)
+
+    if (!priorVote) {
+      await Vote.query().insertAndFetch({
+        value: voteValue,
+        userId: req.user.id,
+        reviewId: reviewId
+      })
+    } else {
+      if(priorVote.value !== voteValue){
+        await Vote.query().findById(priorVote.id).update({value: voteValue})
+      } else {
+        await Vote.query().deleteById(priorVote.id)
+      }
+    }
+    const totalCount = await VoteSerializer.getTotal(reviewId)
+    return res.status(201).json({ totalCount })
+
+  } catch (error) {
+    return res.status(500).json({ errors: error })
+  }
+})
+
+export default voteReviewsRouter
