742e60435483c4a928ecd4776878f11950e6466d
JCWoodworker <jfc3303@gmail.com>
2022-02-10 15:27:12 -0500

    refactored API key

--- server/src/routes/api/weatherRouter.js
+++ server/src/routes/api/weatherRouter.js
@@ -1,13 +1,14 @@
 import express from "express"
 import got from 'got'
 import kelvinConverter from '../../../services/KelvinConverter.js'
-
+import API_KEY from '../../config.js'
 
 const weatherRouter = new express.Router()
 
 weatherRouter.get('/:lat&:long', async (req, res) => {
+  console.log(API_KEY)
   try {
-    const response = await got(`https://api.openweathermap.org/data/2.5/forecast?lat=${req.params.lat}&lon=${req.params.long}&appid=d8e8742fa1b1aa5b85716c6144013e98`)
+    const response = await got(`https://api.openweathermap.org/data/2.5/forecast?lat=${req.params.lat}&lon=${req.params.long}&appid=${API_KEY.API_KEY}`)
     const body = JSON.parse(response.body)
     const city = body.city.name
     const description = body.list[0].weather[0].description
