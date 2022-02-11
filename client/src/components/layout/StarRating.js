ee6939a3e2adcebbb5f80910d40a39782c3175ef
Lilly Fang <lilly.fang@gmail.com>
2022-02-11 11:17:03 -0500

    Fix star rating bug

--- client/src/components/layout/StarRating.js
+++ client/src/components/layout/StarRating.js
@@ -3,17 +3,23 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar, faStarHalf } from "@fortawesome/free-solid-svg-icons"
 
 const StarRating = ({ rating, ratingLabel }) => {
-
+  
   let avgStarRating = []
-  let wholeStars = Math.round(rating)
-  let halfStars = Math.round(rating*2)/2-wholeStars
+  let stars = Math.round(rating*2)/2
+  let wholeStars = Math.floor(stars)
+  let halfStars = 0
+  if (Math.floor(stars)!==stars) {
+    halfStars = 1
+  }
+
   for (let i = 0; i<wholeStars; i++) {
     avgStarRating.push(
       <FontAwesomeIcon icon={faStar} key={i}/>
     )
   }
-  if (halfStars!==0) {
-    avgStarRating.push(avgStarRating.push(<FontAwesomeIcon icon={faStarHalf} key={6}/>))
+
+  if (halfStars>0) {
+    avgStarRating.push(<FontAwesomeIcon icon={faStarHalf} key={6}/>)
   }
 
   return (
